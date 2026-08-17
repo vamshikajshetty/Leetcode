82ff47f8f76eef4de8d7f619f83a288db02e1346
vamshikajshetty <vamshikashetty93@gmail.com>
2026-08-17 19:49:59 +0530

    Time: 1 ms (99.91%), Space: 45.3 MB (66.48%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public int compress(char[] chars) {
+
+        int write = 0;
+        int read = 0;
+
+        while (read < chars.length) {
+
+            char currentChar = chars[read];
+            int count = 0;
+
+            while (read < chars.length && chars[read] == currentChar) {
+                read++;
+                count++;
+            }
+
+            chars[write++] = currentChar;
+
+            if (count > 1) {
+                String countStr = String.valueOf(count);
+
+                for (char c : countStr.toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+        }
+
+        return write;
+    }
+}
